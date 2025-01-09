57f3947f49424d2ff7409a96849d57a7e42c83d0
Krishna Sharma <krishna16sharma.s@gmail.com>
2025-01-09 23:23:03 +0530

    Improved responsiveness

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -29,7 +29,7 @@ const TopButtons = ({setQuery}) => {
     ]
 
   return (
-    <div className='flex items-center justify-around my-6'>
+    <div className='flex flex-wrap items-center justify-around gap-4 my-6'>
         {
             cities.map((city)=>(
                 <button key={city.id} 
